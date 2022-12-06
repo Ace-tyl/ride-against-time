4071e62310c57b02d488407b0aa749382ee8b1e8
Chdyfzh <490252859@qq.com>
2022-12-06 16:48:01 +0800

    Update walker_entity.py

--- walker_entity.py
+++ walker_entity.py
@@ -1,5 +1,15 @@
 import pygame
+import skin
+import gmath
+import numpy as np
 
 
 class WalkerEntity:
-    pass
+    def __init__(self, x, y):
+        self.pos_screen = np.array([x, y])
+    def render(self, screen):
+        nskin = skin.player_skin
+        nskin = pygame.transform.rotate(nskin, gmath.rad_to_deg(self.player_dir))
+        skin_rect = nskin.get_rect()
+        skin_rect.center = self.pos_screen
+        screen.blit(nskin, skin_rect)
